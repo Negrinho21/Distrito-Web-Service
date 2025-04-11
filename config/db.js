b289c213f11a9dcdc0246dde525b48ececcea6f9
Joelson Osmy <joelsonc867@gmail.com>
2025-04-11 08:55:48 +0000

    preparando para fzr deploy

--- config/db.js
+++ config/db.js
@@ -1,9 +1,20 @@
 const {Sequelize} = require('sequelize');
 
 // Connect to the database
-const sequelize = new Sequelize('distrito_db', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
-})
+// const sequelize = new Sequelize('distrito_db', 'root', '', {
+//     host: 'localhost',
+//     dialect: 'mysql',
+// })
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: 'mysql',
+      logging: false
+    }
+  );
+  
 
 module.exports = sequelize;
